3551a3d241549671398dab48c9793cfc0d437435
Anton Parkhomenko <mailbox@chuwy.me>
2015-10-14 13:36:10 +0300

    Add crawler (in a stupid way)

--- loop.hs
+++ loop.hs
@@ -3,10 +3,29 @@ import System.Time
 import System.IO
 import Control.Concurrent
 
-tick = do 
-	now <- fmap show getClockTime
-	threadDelay $ round $ 1000000
-	putStrLn ("Now is: " ++ now)
+-- Derive show because we need to stringify it
+data Direction = Forward | Back deriving Show
 
-main = forever $ tick
-      
+-- Move a dot snake-way
+moveDot :: ((Int, Int), Direction) -> ((Int, Int), Direction)
+moveDot ((x, y), _)       | x == 10 && y == 10 = ((1, 1), Forward)
+moveDot ((x, y), Forward) | x == 10            = ((x, y + 1), Back)
+moveDot ((x, y), Back)    | x == 1             = ((x, y + 1), Forward)
+moveDot ((x, y), Forward)                      = ((x + 1, y), Forward)
+moveDot ((x, y), Back)                         = ((x - 1, y), Back)
+
+-- always return same given value but with delay
+addDelay :: Show a => a -> IO a
+addDelay x = fmap constant delay
+    where delay      = threadDelay 1000000
+          constant _ = x
+
+-- output position and move forward
+keepCrawling :: ((Int, Int), Direction) -> IO Int
+keepCrawling position = do
+    current <- addDelay position
+    putStrLn $ show current
+    keepCrawling $ moveDot position
+
+main = keepCrawling start
+    where start = ((1, 1), Forward)
